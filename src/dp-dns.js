49ec385abc7590abe8b529eb7719cd9db3b4897e
freemany <freeman_yam@hotmail.com>
2019-03-21 09:25:43 +1100

    test removing main

--- src/dp-dns.js
+++ src/dp-dns.js
@@ -4,5 +4,5 @@ const cmd = require('commander');
 cmd
     .description('DNS tool')
     .command('show', 'show current DNS').alias('get')
-    .command('set', 'set local DNS')
+    .command('set', 'set DNS')
     .parse(process.argv);
